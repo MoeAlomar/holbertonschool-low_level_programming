d65e0e592eb9b0353030b294e13d69e5e1b1d686
MoeAlomar <Mohammedahalomar@gmail.com>
2025-02-16 11:32:00 +0000

    T

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char count;
+
+	for (count = '0'; count <= '9'; count++)
+{
+	putchar(count + ',' + ' ');
+}
+}
