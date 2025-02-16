82eae25850ffa7b33ebaf3a4654ce973edcb32a9
MoeAlomar <Mohammedahalomar@gmail.com>
2025-02-16 11:12:16 +0000

    T

---
+++ variables_if_else_while/6-print_numbrez.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count = 48;
+
+	for (count = 48; count <= 57; count++)
+{
+	putchar(count);
+}
+	putchar('\n');
+	return (0);
+}
