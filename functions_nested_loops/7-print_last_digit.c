81a1fffdb117ad009bab2fe732bce4e976fc4681
MoeAlomar <Mohammedahalomar@gmail.com>
2025-02-17 10:00:49 +0000

    T

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,9 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
-	n = -n;
+	num = -((unsigned int)n);
+	else
+	num = n;
 	last_digit = (n % 10);
 	_putchar(last_digit + '0');
 	return (last_digit);
