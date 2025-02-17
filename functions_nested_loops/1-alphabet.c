b4d590769644d92a809ae0b2e2e2b5da215869aa
MoeAlomar <Mohammedahalomar@gmail.com>
2025-02-17 08:19:40 +0000

    T

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * print_alphabet - This method prints alphabets in small letter from a-z
+ *
+ * @void this method returns nothing.
+ */
 void print_alphabet(void)
 {	char i;
 
