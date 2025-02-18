d568ec8b8dc444ca5874d5e102a34ab54a82a358
MoeAlomar <Mohammedahalomar@gmail.com>
2025-02-18 11:28:01 +0000

    T

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * times_table - this method shows the times table from 0-9
+ *Return: this method returns nothing.
+ */
+void times_table(void)
+{
+	int i;
+
+	int j;
+
+	int product;
+
+	for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+{
+	product = i * j;
+	if (product >= 10)
+	_putchar((product / 10) + '0');
+	_putchar((product % 10) + '0');
+	if (j < 9)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+}
+}
+	_putchar('\n');
+}
+}
