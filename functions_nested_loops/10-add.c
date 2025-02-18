82e41ffa8717819f4879b3ffce66835b99e4893d
MoeAlomar <Mohammedahalomar@gmail.com>
2025-02-18 11:46:50 +0000

    T

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - this function recieves two integers together and retuns them
+ * @a: first integer to be summed
+ * @b: second integer to be summed
+ *Return: it returns integer of summing to integers
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
