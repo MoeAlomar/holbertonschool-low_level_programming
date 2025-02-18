4399e02cef2fbc3222710c9f1867ab1cc39062ca
MoeAlomar <Mohammedahalomar@gmail.com>
2025-02-18 12:27:58 +0000

    T

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -16,9 +16,9 @@ void print_times_table(int n)
 
 	if (n < 0 || n > 15)
 	return;
-	for (i = n; i <= n; i++)
+	for (i = 0; i <= n; i++)
 {
-	for (j = n; j <= n; j++)
+	for (j = 0; j <= n; j++)
 {
 	product = i * j;
 
