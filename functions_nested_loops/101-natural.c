9a14c77b96b9a6a793cbcccc949e90f8292ba56f
MoeAlomar <Mohammedahalomar@gmail.com>
2025-02-19 08:33:30 +0000

    T

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
- * main - this method is the start point of the program.
  * print_natural - this method prints all natural numbers that are
  * Return: this method of type void so it returns nothing.
  */
@@ -20,8 +19,12 @@ void print_natural(void)
 }
 	printf("%ld\n", sum);
 }
+/**
+ * main - this is the start point of the program.
+ * Return: this returns nothing.
+ */
 int main(void)
 {
 	print_natural();
-	return(0);
+	return (0);
 }
