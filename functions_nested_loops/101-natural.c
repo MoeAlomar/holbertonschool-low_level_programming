24988652131ab79ffd1f4b57209a6152009723e9
MoeAlomar <Mohammedahalomar@gmail.com>
2025-02-19 08:05:56 +0000

    T

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_natural - this method prints all natural numbers that are 
+ * multiplies of 3 or 5 and prints their sums until 1024 excluded.
+ * Return: this method of type void so it returns nothing.
+ */
+void print_natural(void)
+{
+	int num = 1024;
+
+	int i,sum;
+
+ for (i = 1; i < num; i++)
+{
+  if (i % 3 == 0 || i % 5 == 0)
+{
+	sum += i;
+}
+}
+	printf("%d", sum);
+}
