0c61d9e756070e0d93e85088365e32e2710d7357
MoeAlomar <Mohammedahalomar@gmail.com>
2025-02-25 11:09:59 +0000

    T

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,22 +2,24 @@
 /**
  * _isupper - this functios return 1 if a char is upper or 0 if lower
  * @c: this is the parameter that recieves an integer as an ASCII Code
- *Return this method return 1 or 0 if upper or lower 
+ *Return: this method return 1 or 0 if upper or lower
  */
 int _isupper(int c)
 {
-  if (c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 {
 	_putchar(c + '0');
-	_putchar(": ");
+	_putchar(':');
 	_putchar('1');
+	_putchar('\n');
 	return (1);
 }
 	else
 {
 	_putchar(c + '0');
-	_putchar(": ");
-	_putchar('1');
+	_putchar(':');
+	_putchar('0');
+	_putchar('\n');
 	return (0);
 }
 }
