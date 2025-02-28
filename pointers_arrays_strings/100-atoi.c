e2afce7a78e8726f90b0c6b2cc677934b4e2d904
MoeAlomar <Mohammedahalomar@gmail.com>
2025-02-28 19:31:41 +0000

    T

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,43 +4,46 @@
  * _atoi - Converts a string to an integer.
  * @s: Pointer to the string to be converted.
  *
- * Return: The integer representation of the string. If no numbers are found,
- *         returns 0. If conversion overflows, returns INT_MAX or INT_MIN.
+ * Description: This function converts a string to an integer,
+ * considering all the '-' and '+' signs that precede the number.
+ * If no number is found, it returns 0. It also checks for overflow
+ * and returns INT_MAX or INT_MIN accordingly.
+ *
+ * Return: The integer representation of the string.
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    int result = 0;
-    int digit;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int digit;
 
-    /* Process the string until a digit is encountered.
-     * Count every '-' and '+' sign that appears before the first digit.
-     */
-    while (s[i])
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        else if (s[i] >= '0' && s[i] <= '9')
-            break;
-        /* For '+' or any other character, simply continue */
-        i++;
-    }
+	/* Process the string until a digit is encountered.
+	 * Count every '-' and '+' sign before the first digit.
+	 */
+	while (s[i])
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+			break;
+		i++;
+	}
 
-    /* If no digit is found, return 0 */
-    if (!(s[i] >= '0' && s[i] <= '9'))
-        return 0;
+	/* If no digit is found, return 0 */
+	if (!(s[i] >= '0' && s[i] <= '9'))
+		return (0);
 
-    /* Convert the sequence of digit characters into an integer */
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        digit = s[i] - '0';
-        /* Check for overflow before updating the result */
-        if (result > (INT_MAX - digit) / 10)
-            return (sign == 1) ? INT_MAX : INT_MIN;
-        result = result * 10 + digit;
-        i++;
-    }
+	/* Convert the sequence of digit characters into an integer */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		digit = s[i] - '0';
+		/* Check for overflow before updating the result */
+		if (result > (INT_MAX - digit) / 10)
+			return ((sign == 1) ? INT_MAX : INT_MIN);
+		result = result * 10 + digit;
+		i++;
+	}
 
-    return result * sign;
+	return (result * sign);
 }
