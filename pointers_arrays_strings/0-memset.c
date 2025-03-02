332b241452ada67762c743ce700fc53fd59eb5d1
MoeAlomar <Mohammedahalomar@gmail.com>
2025-03-02 20:10:11 +0000

    T

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+/**
+ * _memset - this functions fills the first n Bytes pointed by
+ * a pointer with char b
+ * @s: this is the pointer s
+ * @b: this is the char b to be filled in the pointer spaces
+ * @n: this is the number of bytes that has to be filled with b
+ * Return: this method return the pointer itself
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+{
+	*s = b;
+	s++;
+}
+	return (*s);
+}
