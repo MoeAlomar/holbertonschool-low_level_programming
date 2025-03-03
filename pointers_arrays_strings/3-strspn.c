7df662e0373517eba8992f7a1b68ab6197f7044e
MoeAlomar <Mohammedahalomar@gmail.com>
2025-03-03 20:37:25 +0000

    T

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,13 @@
 /**
- * _strspn - this method counts the number of a char repeated from string to another
+ * _strspn - this method counts the number of a char
+ * repeated from string to another
  * @s: is the string we want to compare prefix in
- * @accept: this is the string that we will be comparing to the first string
+ * @accept: this is the string that we will be
+ * comparing to the first string
+ * Return: we return the length of the prefix
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i,j;
 	unsigned int prefixLngth;
 	char *tmp = s;
 
@@ -15,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 	while (*s != '\0')
 {
 	if (*accept == *s)
-	prefixLgnth++;
+	prefixLngth++;
 	s++;
 }
 	accept++;
