06d374671d60c6b5ec5e0882a8c7ac6e5176da48
MoeAlomar <Mohammedahalomar@gmail.com>
2025-03-05 10:49:45 +0000

    T

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+/**
+ * _strchr - this method finds the first occurance of a char
+ * @s: this is the pointer of the char
+ * @c: this is the char we are looking for in the string
+ * Return: this method returns the pointer when the char first occured
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (*s != '\0')
+{
+	if (*s == c)
+	return (s);
+	s++;
+}
+	return (NULL);
+}
