33373f2721dc2a98069b4a87f1d80bfbd41337d9
MoeAlomar <Mohammedahalomar@gmail.com>
2025-03-06 14:10:38 +0000

    T

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,11 +6,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	int count = 1;
+	if (*s == '\0')
+	return (0);
 
-	if (*(s + 1) != '\0')
-{
-	count += _strlen_recursion(s + 1);
-}
-	return (count);
+	return (1 + _strlen_recursion(s + 1));
 }
