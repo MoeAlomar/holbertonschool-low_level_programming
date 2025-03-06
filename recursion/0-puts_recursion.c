02939490029d927d90b35c612a118154907c764a
MoeAlomar <Mohammedahalomar@gmail.com>
2025-03-06 13:21:40 +0000

    T

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - this method simulates puts but with recursion
+ * @s: this is the string pointer we will print
+ * Return: this method returns void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s);
+}
+	_putchar('\n');
+}
