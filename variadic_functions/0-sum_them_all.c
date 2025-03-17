ee1c23da8566950842f7b5f1239582149c8b3008
MoeAlomar <Mohammedahalomar@gmail.com>
2025-03-17 10:51:11 +0000

    Task

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,4 +1,3 @@
-
 #include <stdarg.h>
 
 /**
@@ -8,17 +7,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    unsigned int i;
-    int sum = 0;
-    va_list args;
+	unsigned int i;
+	int sum = 0;
+	va_list args;
 
-    if (n == 0)
-        return (0);
+	if (n == 0)
+		return (0);
 
-    va_start(args, n);
-    for (i = 0; i < n; i++)
-        sum += va_arg(args, int);
-    va_end(args);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
 
-    return (sum);
+	return (sum);
 }
